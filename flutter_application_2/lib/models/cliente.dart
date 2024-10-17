e933a21ca9c0e2bb0c33b20b786d504eed61a781
alanBMC <alanbmc00@gmail.com>
2024-10-17 09:31:13 -0400

    criando classe cliente

---
+++ flutter_application_2/lib/models/cliente.dart
@@ -0,0 +1,13 @@
+class Cliente {
+  final int usuarioId;
+  final String nome;
+  final String telefone;
+  final String foto;
+
+  const Cliente({
+    required this.usuarioId,
+    required this.nome,
+    required this.telefone,
+    required this.foto
+  });
+}
