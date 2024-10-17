1e7b51e0ae2c89b23297a40e4fbb84fa115c2a1f
alanBMC <alanbmc00@gmail.com>
2024-10-17 11:01:17 -0400

    feat: criando classes resposta & alternativa

---
+++ flutter_application_2/lib/models/respostaTemAlternativa.dart
@@ -0,0 +1,29 @@
+class RespostaTemAlternativa {
+  final int respostaId;
+  final int alternativaId;
+  final Resposta resposta;
+  final Alternativa alternativa;
+
+  RespostaTemAlternativa({
+    required this.respostaId,
+    required this.alternativaId,
+    required this.resposta,
+    required this.alternativa,
+  });
+
+  factory RespostaTemAlternativa.fromJson(Map<String, dynamic> json) {
+    return RespostaTemAlternativa(
+      respostaId: json['respostaId'],
+      alternativaId: json['alternativaId'],
+      resposta: Resposta.fromJson(json['resposta']),
+      alternativa: Alternativa.fromJson(json['alternativa']),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'respostaId': respostaId,
+        'alternativaId': alternativaId,
+        'resposta': resposta.toJson(),
+        'alternativa': alternativa.toJson(),
+      };
+}
