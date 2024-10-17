5eeede3fb29ce06bdc382cc85b0d64d8430bced6
alanBMC <alanbmc00@gmail.com>
2024-10-17 11:03:10 -0400

    feat: ajustando classe cliente

--- flutter_application_2/lib/models/cliente.dart
+++ flutter_application_2/lib/models/cliente.dart
@@ -1,27 +1,49 @@
 class Cliente {
+  final int id;
   final int usuarioId;
-  final String nome;
-  final String telefone;
-  final String foto;
+  final String? nome;
+  final String? telefone;
+  final String? foto;
+  final Usuario usuario;
+  final List<Interacao>? interacoes;
+  final List<ClienteRespondeCheckList>? clienteResponde;
 
-  const Cliente({
+  Cliente({
+    required this.id,
     required this.usuarioId,
-    required this.nome,
-    required this.telefone,
-    required this.foto
+    this.nome,
+    this.telefone,
+    this.foto,
+    required this.usuario,
+    this.interacoes,
+    this.clienteResponde,
   });
 
-  factory Cliente.fromJson(Map<String, dynamic> json) => Cliente(
-    usuarioId: json['usuarioId'],
-    nome: json['nome'],
-    telefone: json['telefone'], 
-    foto: json['foto'],
-  );
+  factory Cliente.fromJson(Map<String, dynamic> json) {
+    return Cliente(
+      id: json['id'],
+      usuarioId: json['usuarioId'],
+      nome: json['nome'],
+      telefone: json['telefone'],
+      foto: json['foto'],
+      usuario: Usuario.fromJson(json['usuario']),
+      interacoes: (json['interacoes'] as List<dynamic>?)
+          ?.map((item) => Interacao.fromJson(item))
+          .toList(),
+      clienteResponde: (json['clienteResponde'] as List<dynamic>?)
+          ?.map((item) => ClienteRespondeCheckList.fromJson(item))
+          .toList(),
+    );
+  }
 
   Map<String, dynamic> toJson() => {
-     "usuarioId": usuarioId,
-     "nome": nome,
-     "telefone": telefone,
-     "foto": foto,
-  };
+        'id': id,
+        'usuarioId': usuarioId,
+        'nome': nome,
+        'telefone': telefone,
+        'foto': foto,
+        'usuario': usuario.toJson(),
+        'interacoes': interacoes?.map((item) => item.toJson()).toList(),
+        'clienteResponde': clienteResponde?.map((item) => item.toJson()).toList(),
+      };
 }
